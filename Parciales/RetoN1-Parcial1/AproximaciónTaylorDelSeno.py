d66fb157282de97f9ec16d1b253bcf991156a091
sofiamoreno199 <53268509+sofiamoreno199@users.noreply.github.com>
2020-02-25 07:48:51 -0500

    Update AproximaciónTaylorDelSeno.py

--- Parciales/RetoN1-Parcial1/AproximaciónTaylorDelSeno.py
+++ Parciales/RetoN1-Parcial1/AproximaciónTaylorDelSeno.py
@@ -1,4 +1,5 @@
 #Hecho por Sofia Moreno y Anggie Correa
+#Hecho por Sofia Moreno y Anggie Correa
 
 import numpy as np
 import sympy as sym
@@ -16,20 +17,43 @@ def politaylor(funcionx,x0,n):
         k = k + 1
     return(polinomio)
 
+def error (x, aproximacion):
+  if math.sin(x) == 0:
+    return(0)
+  else: 
+    errorAbsoluto = math.sin(x) - aproximacion
+    errorRelativo = (errorAbsoluto/math.sin(x))*100
+    return (errorRelativo)
+  x=x+x
+
+def operacionesNecesarias(poliniomio):
+  op=0
+  for i in range(poliniomio):  
+    op = op + 1
+  return (op)
+
+
 x = sym.Symbol('x')
 funcionx = sym.sin(x) 
-
+errorTota=0
+operaciones=0
 x0 = 0          
 n  = 10         # Grado polinomio Taylor
 a  = -math.pi/64 ; b = math.pi/64  # x entre [a,b]
-muestras = 51
+
 
 op=0
 px_tabla = []
+datos=[]
 for grado in range(0,n,1):
     polinomio = politaylor(funcionx,x0,grado)
     px_tabla.append(polinomio)
     op=op+1
+g=(math.pi/64 + math.pi/64) /n
+for i in range (0,n,1):
+  datos.append(sym.sin(g))
+  g=g+g
+
 
 for grado in range(0,n,1):
     px = px_tabla[grado]
@@ -38,4 +62,7 @@ for grado in range(0,n,1):
     print('polinomio: ')
     sym.pprint(px)
     print()
-print ("El número de operaciones es de: "+str(op*10))
+    errorTota = errorTota + error(g, datos[grado]) 
+    operaciones = operaciones + 5
+    print("El error toal es:"+str(errorTota))
+print ("El número de operaciones es de: "+str(operaciones))
